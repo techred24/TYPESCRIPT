91e077933c90fb55ea0cb626d2a04064b7d565d6
TeChRF24 <f.rafael.temich24@gmail.com>
2022-06-30 19:08:03 -0500

    Exercise. Question mark in object

--- src/classes/Hero.ts
+++ src/classes/Hero.ts
@@ -1,3 +1,4 @@
+import powers from "../data/powers";
 
 
 export class Hero {
@@ -6,6 +7,11 @@ export class Hero {
         public powerId: number,
         public age: number
     ) {}
+
+    get power(): string { // return string
+        // console.log(powers.find( power => power.id === this.powerId )?.desc, 'mark');
+        return powers.find( power => power.id === this.powerId )?.desc || 'not found';
+    }
 }
 
 export const PI = 3.1416;
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 // import { Hero as SuperHero } from "./classes/Hero";
-import  * as HeroClasses  from "./classes/Hero";
+// import  * as HeroClasses  from "./classes/Hero";
 // import { Power } from "./data/powers";
-import poderes, { Power } from "./data/powers";
+// import poderes, { Power } from "./data/powers";
+import { Hero } from './classes/Hero';
 
 
-const Hero = 123;
+// const Hero = 123;
 
-const ironman = new HeroClasses.Hero('Ironman', 1, 55);
+const ironman = new Hero('Ironman', 10, 55);
 
-console.log( ironman );
+console.log( ironman.power );
 
-console.log( poderes[0].desc );
+// console.log( poderes[0].desc );
